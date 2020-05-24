9d636fe43ee025277ea1e566aa2f513cab86cc91
Rhys Braginton Pettee Olsen <rhol9958@colorado.edu>
2020-05-24 17:07:47 -0600

    Update quick-fix.sh

--- quick-fix.sh
+++ quick-fix.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-echo "Printing /etc/fstab"
-cat /etc/fstab
-echo "Printing /etc/fstab.backup"
-cat /etc/fstab.backup
-mv /etc/fstab /etc/fstab.backup2
-mv /etc/fstab.backup /etc/fstab
+mount -o remount,rw /tmp
